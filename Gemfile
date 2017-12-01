97afa3ff51642e22b4b9e628daa48fbdd7681fa6
Dan Actor <danltactor@gmail.com>
2017-12-01 10:08:50 -0700

    complete

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 ruby '2.3.1'
 
-require 'pry'
-require 'colorize'
+gem 'pry'
+gem 'colorize'
+gem 'to_words', '~> 1.1', '>= 1.1.1'
---
+++ magicnumber.rb
@@ -0,0 +1,23 @@
+require 'colorize'
+require 'pry'
+require 'to_words'
+
+def menu
+  print `clear`
+  puts "--4 is the magic number--".yellow
+  puts "Enter any postive number or type quit:".yellow
+  @number = STDIN.gets.strip.to_i
+  magic
+end
+
+def magic
+  puts "#{@number} is #{@number.to_words.split("").length}"
+  @number = @number.to_words.split("").length
+  if @number == 4
+    print "And 4 is the magic number"
+  else
+    magic
+  end
+end
+
+menu
